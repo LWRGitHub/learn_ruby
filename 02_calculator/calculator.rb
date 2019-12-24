11b0aaad4e3bad993c6661aef256f88cb6e21dc3
LWRGitHub <loganreynoldslwr@gmail.com>
2019-12-24 02:37:19 +0000

    completed 3 of 7 exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
-#write your code here
+
+def add(num1, num2)
+    sum = num1 + num2
+    return sum
+end
+
+def subtract(num1, num2)
+    sum = num1 - num2
+    return sum
+end
+
+def sum(arr)
+    i = 0
+    sum = 0
+    if arr.length == 0
+        return 0
+    else
+        while i < arr.length
+            sum = sum + arr[i]
+            i = i + 1
+        end
+    end
+    return sum
+end
