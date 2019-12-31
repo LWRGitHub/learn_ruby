8016467efc9122279474ebc72eb36ebb3fc4a52f
LWRGitHub <loganreynoldslwr@gmail.com>
2019-12-31 23:00:04 +0000

    completed 5 of 7 exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,29 +1,30 @@
 class Book
-# write your code here
-end
-
-def title(str)
+  attr_accessor :title
+  def capitalized_title
+    @title = title
     #turns all 1st letter capitalezed
-    str = str.split.map(&:capitalize).join(' ')
+    @title = @title.split.map(&:capitalize).join(' ')  
     #loops through string uses if statment to find the location "i" of the space
-    for i in 1..str.length do
-        if str[i] == " "
-            if "And" == str.slice((i + 1), 3)
-                str[i + 1] = "a"
-            elsif "The" == str.slice((i + 1), 3)
-                str[i + 1] = "t"
-            elsif "Over" == str.slice((i + 1), 4)
-                str[i + 1] = "o"
-            elsif "An" == str.slice((i + 1), 2)
-                str[i + 1] = "a"
-            elsif "A" == str.slice((i + 1), 1)
-                str[i + 1] = "a"
-            elsif "Of" == str.slice((i + 1), 2)
-                str[i + 1] = "o"
-            elsif "In" == str.slice((i + 1), 2)
-                str[i + 1] = "i"
+    for i in 1..@title.length do
+        if @title[i] == " "
+            if "And" == @title.slice((i + 1), 3)
+                @title[i + 1] = "a"
+            elsif "The" == @title.slice((i + 1), 3)
+                @title[i + 1] = "t"
+            elsif "Over" == @title.slice((i + 1), 4)
+                @title[i + 1] = "o"
+            elsif "An" == @title.slice((i + 1), 2)
+                @title[i + 1] = "a"
+            elsif "A " == @title.slice((i + 1), 2)
+                @title[i + 1] = "a"
+            elsif "Of" == @title.slice((i + 1), 2)
+                @title[i + 1] = "o"
+            elsif "In " == @title.slice((i + 1), 3)
+                @title[i + 1] = "i"
             end
         end
     end
-    return str
+    return @title
+  end
 end
+
